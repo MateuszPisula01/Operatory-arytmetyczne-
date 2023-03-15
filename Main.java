3be54fa503117b78fc69c5755ad24e8979d5de02
MateuszPisula01 <126687627+MateuszPisula01@users.noreply.github.com>
2023-03-15 08:45:48 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,21 @@
+class Main {
+  public static void main(String[] args) {
+
+int a=20;
+    int b= 10;
+
+  // reszta z dzielenia %
+    int wynik = a%b;
+   a+=b;
+    
+    System.out.println(wynik);
+     System.out.println(a);
+    // Potęga pow
+    // pierwiastkowanie sqrt
+    // wartosc bezwzględna abs 
+  // inkrementacja a++
+    //dekrementacja a--
+    
+    
+  }
+}
